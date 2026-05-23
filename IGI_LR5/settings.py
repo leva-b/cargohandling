96f77cb720d73568f5bdaf0b1698e3d67fa39dcc
leva-b <levchishin0@gmail.com>
2026-05-23 20:19:04 +0300

    Update settings.py

--- IGI_LR5/settings.py
+++ IGI_LR5/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g8)+ks4srt2$kt^2*u900v!-k=p3ggh)x4igedj3_^bqt-=1d-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
+ALLOWED_HOSTS = [*]
 USER_TIME_ZONE = 'Europe/Minsk'
 
 
